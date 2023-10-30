f5a6ef329f933e645f5aab3cebe006cee16b5501
SteffenHub <steffenhub@users.noreply.github.com>
2023-10-30 15:58:04 +0100

    simplified some functionalities by creating separate functions.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.TimeoutException;
 
 import java.math.BigInteger;
@@ -53,14 +54,14 @@ public class Main {
             }
             System.out.println("Fuege Regel hinzu: " + Arrays.toString(neueRegel));
             rules.add(neueRegel);
-            try {
-                satSolver.addRule(neueRegel);
-            } catch (Exception e) {
+
+            if (!checkForSatisfibiltiy(satSolver, neueRegel)) {
                 System.err.println("Diese Regel erzeugt eine Kontradiktion, ich nehme sie wieder raus");
                 rules.remove(neueRegel);
                 satSolver = new SatSolver(rules);
                 continue;
             }
+
             int falseVarsBisJetzt = countFalseVars(satSolver, iD.numberOfVariables);
             if (!(falseVarsBisJetzt <= iD.falseVars)) {
                 System.err.println("Diese Regel würde zu viele variablen ausschließen und nicht mehr möglich machen diese zu wählen: " + falseVarsBisJetzt + "/" + iD.falseVars);
@@ -75,58 +76,65 @@ public class Main {
                 satSolver = new SatSolver(rules);
                 continue;
             }
-            try {
-                varianz = Operation.getVariance(rules, iD.numberOfVariables);
-                System.out.println("Die Varianz mit der Regeln ist nun: " + Operation.punkteInBigInt(varianz));
-                System.out.println("Es Fehlen noch: " + Operation.punkteInBigInt(varianz.subtract(iD.goalVariance)));
-                if (varianz.compareTo(iD.goalVariance.subtract(iD.goalVarianceDeviation)) < 0) {
-                    System.err.println("das drückt die Varianz zu doll ich nehme die Regel wieder raus");
-                    rules.remove(neueRegel);
-                    satSolver = new SatSolver(rules);
-                    wirdWiederGeloescht = true;
-                } else if (varianz.compareTo(iD.goalVariance.subtract(iD.goalVarianceDeviation)) > 0 && varianz.compareTo(iD.goalVariance.add(iD.goalVarianceDeviation)) < 0) {
-                    System.out.println("Ich habe ein Regelwerk gefunden, dass die Varianz erfüllt");
-                    machWeiter = false;
-                }
-            } catch (Exception e) {
-                System.err.println("Beim finden der Varianz ist etwas beim c2d schief gelaufen. Ich nehme diese und die vorherige Regel wieder raus");
+            varianz = getVariance(rules, iD.numberOfVariables);
+            if (varianz.compareTo(new BigInteger("-1")) == 0) {
+                System.err.println("Something went wrong with c2d when finding the variance. I am taking this rule out again");
                 rules.remove(neueRegel);
-                rules.remove(rules.size() - 1);
                 satSolver = new SatSolver(rules);
-                wirdWiederGeloescht = true;
+                continue;
             }
-            if (!wirdWiederGeloescht) {
-                System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
-                System.out.println();
-                System.out.println("Die nehmen wir");
-                System.out.println();
-                System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+            System.out.println("The variance with the rules is now: " + Operation.punkteInBigInt(varianz));
+            System.out.println("Still missing: " + Operation.punkteInBigInt(varianz.subtract(iD.goalVariance)));
+            // If the variance is too small
+            if (varianz.compareTo(iD.goalVariance.subtract(iD.goalVarianceDeviation)) < 0) {
+                System.err.println("das drückt die Varianz zu doll ich nehme die Regel wieder raus");
+                rules.remove(neueRegel);
+                satSolver = new SatSolver(rules);
+                continue;
             }
+            // If an optimal Variance is reached -> Stop calculation
+            if (varianz.compareTo(iD.goalVariance.subtract(iD.goalVarianceDeviation)) > 0 && varianz.compareTo(iD.goalVariance.add(iD.goalVarianceDeviation)) < 0) {
+                System.out.println("Ich habe ein Regelwerk gefunden, dass die Varianz erfüllt");
+                machWeiter = false;
+            }
+            System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+            System.out.println();
+            System.out.println("Die nehmen wir");
+            System.out.println();
+            System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
             System.out.println("-----------------------------------------------------------------------------");
         }
 
-        List<String> fileOutput = new ArrayList<>();
-        fileOutput.add("c ");
-        fileOutput.add("c Input Variance: " + iD.goalVariance);
-        fileOutput.add("c Actual Variance: " + varianz);
-        fileOutput.add("c Input number of vars : " + iD.numberOfVariables);
-        fileOutput.add("c Input Fam size: " + iD.minFamilySize + "-" + iD.maxFamilySize);
-        fileOutput.add("c Input Rule size: " + iD.minRuleSize + "-" + iD.maxRuleSize);
-        fileOutput.add("c Input False Variables: " + iD.falseVars);
-        fileOutput.add("c Actual False Variables: " + countFalseVars(satSolver, iD.numberOfVariables) + " Vars");
-        fileOutput.add("c Input True Variables: " + iD.trueVars);
-        fileOutput.add("c Actual True Variables: " + countTrueVars(satSolver, iD.numberOfVariables) + " Vars");
-        fileOutput.add("c ");
-        fileOutput.add("p cnf " + iD.numberOfVariables + " " + rules.size());
-        for (int[] regel : rules) {
-            fileOutput.add(Arrays.toString(regel).replace("[", "").replace("]", "").replace(",", "") + " 0");
-        }
+        List<String> fileOutput = TxtConverter.convertRulesToStringListCNF(rules, iD, varianz, countFalseVars(satSolver,
+                iD.numberOfVariables), countTrueVars(satSolver, iD.numberOfVariables));
         for (String line : fileOutput) {
             System.out.println(line);
         }
         TxtReaderWriter.writeListOfStrings("cnfBuilder" + iD.numberOfVariables + "Vars" + "Variance" + varianz + ".txt", fileOutput);
+    }
 
+    public static BigInteger getVariance(List<int[]> allRules, int numberOfVariables) {
+        try {
+            return Operation.getVariance(allRules, numberOfVariables);
+        } catch (Exception e) {
+            return new BigInteger("-1");
+        }
+    }
 
+    /**
+     * Tries to add the new Rule to the SatSolver
+     *
+     * @param satSolver The SatSolver filled with all previous rules
+     * @param newRule   The new Rule that should be testet
+     * @return true if rule is added and satisfiable. False if adding Rule ends in a Contradiction
+     */
+    public static boolean checkForSatisfibiltiy(SatSolver satSolver, int[] newRule) {
+        try {
+            satSolver.addRule(newRule);
+            return true;
+        } catch (ContradictionException e) {
+            return false;
+        }
     }
 
     public static int countFalseVars(SatSolver satSolver, int anzahlVariablen) throws TimeoutException {
--- src/TxtConverter.java
+++ src/TxtConverter.java
@@ -1,4 +1,6 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TxtConverter {
@@ -46,6 +48,27 @@ public class TxtConverter {
             cnfArr.add(klausel);
         }
         return cnfArr;
+    }
+
+    public static List<String> convertRulesToStringListCNF(List<int[]> rules, InputData inputData, BigInteger variance,
+                                                           int actualFalseVars, int actualTrueVars) {
+        List<String> fileOutput = new ArrayList<>();
+        fileOutput.add("c ");
+        fileOutput.add("c Input Variance: " + inputData.goalVariance);
+        fileOutput.add("c Actual Variance: " + variance);
+        fileOutput.add("c Input number of vars : " + inputData.numberOfVariables);
+        fileOutput.add("c Input Fam size: " + inputData.minFamilySize + "-" + inputData.maxFamilySize);
+        fileOutput.add("c Input Rule size: " + inputData.minRuleSize + "-" + inputData.maxRuleSize);
+        fileOutput.add("c Input False Variables: " + inputData.falseVars);
+        fileOutput.add("c Actual False Variables: " + actualFalseVars + " Vars");
+        fileOutput.add("c Input True Variables: " + inputData.trueVars);
+        fileOutput.add("c Actual True Variables: " + actualTrueVars + " Vars");
+        fileOutput.add("c ");
+        fileOutput.add("p cnf " + inputData.numberOfVariables + " " + rules.size());
+        for (int[] regel : rules) {
+            fileOutput.add(Arrays.toString(regel).replace("[", "").replace("]", "").replace(",", "") + " 0");
+        }
+        return fileOutput;
     }
 
 }
